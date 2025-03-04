e4ba7a7f97ef5e52dc2ddd57d3d6581691cfdc9a
ale sierra <malesierra15@gmail.com>
2025-03-04 16:02:50 -0600

    metodo cargar que reconoce expresiones no validas y reconoce el tipo de expresion

--- main/InterpreteLisp.java
+++ main/InterpreteLisp.java
@@ -24,8 +24,76 @@ public class InterpreteLisp {
     /**public String evaluar (Expresion expr) {
     *    return expr.evaluar();
     *}
-    *
-    *public void cargar (String) para ver a que clase pertenece la expresion
     */
+    public void cargar (String entrada) {
+        entrada = entrada.trim();
+        String[] tokens = entrada.split("\\s+"); // separa por espacios
+        if (tokens.length == 0) {
+            System.out.println("Entrada vacía");
+        }
+
+        String operador = tokens [0];
+        switch (operador) {
+            case "set":
+            case "setq":
+                if (tokens.length != 3) {
+                    System.out.println("Error de sintaxis en set");
+                } else {
+                    definirVariable(tokens[1], tokens[2]);
+                    System.out.println("Variable definida: " + tokens[1]);
+                } break;
+            case "quote":
+            case "":
+                if (tokens.length != 2) {
+                    System.out.println("Error de sintaxis en quote");
+                } else {
+                    System.out.println("Resultado quote: " + tokens[1]);
+                } break;
+            case "defun":
+            if (tokens.length < 4) {
+                System.out.println("Error de sintaxis en defun");
+            } else {
+                String nombre = tokens[1];
+                String[] parametros = new String[tokens.length - 3];
+                for (int i = 0; i < parametros.length; i++) {
+                    parametros[i] = tokens[i + 2];
+                }
+                String valor = tokens[tokens.length - 1];
+                System.out.println("Función definida: " + nombre);
+            } break;
+            case "cond":
+                if (tokens.length < 3) {
+                    System.out.println("Error de sintaxis en cond");
+                } else {
+                    Expresion condicional = new ExCond(entrada);
+                    System.out.println("Resultado:" + condicional.evaluar());
+                } break;
+            case "atom":
+            case "list":
+            case "equal":
+            case "<":
+            case ">":
+                if (tokens.length < 2) {
+                    System.out.println("Error de sintaxis en " + operador);
+                } else {
+                    Expresion predicado = new ExPredicado(entrada);
+                    System.out.println("Resultado: " + predicado.evaluar());
+                } break;
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                if (tokens.length != 3) {
+                    System.out.println("Error de sintaxis en " + operador);
+                } else {
+                    Expresion operacion = new ExOperacion(entrada);
+                    System.out.println("Resultado: " + operacion.evaluar());
+                } break;
+            default :
+                System.out.println("Operador no reconocido: " + operador);
+                break;
+        }
+    } 
+    
 
 }
