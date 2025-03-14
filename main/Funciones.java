20d9388b80cecfcde66962cef008c3b02c7da121
ale sierra <malesierra15@gmail.com>
2025-03-13 22:42:37 -0600

    Clase Funciones

--- main/Funciones.java
+++ main/Funciones.java
@@ -1,1 +1,54 @@
+public class Funciones {
+
+    private String nombreFuncion;   
+    private String[] parametros;    
+    private String cuerpo;          
+
+    public Funciones(String nombreFuncion, String[] parametros, String cuerpo) {
+        this.nombreFuncion = nombreFuncion;
+        this.parametros = parametros;
+        this.cuerpo = cuerpo;
+    }
+
+    public String getNombreFuncion() {
+        return nombreFuncion;
+    }
+
+    public String[] getParametros() {
+        return parametros;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setNombreFuncion(String nombreFuncion) {
+        this.nombreFuncion = nombreFuncion;
+    }
+
+    public void setParametros(String[] parametros) {
+        this.parametros = parametros;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    public String invocar(String[] argumentos) {
+        if (argumentos.length != parametros.length) {
+            return "Error: número incorrecto de argumentos.";
+        }
+    
+        String resultado = "Invocando la función: " + nombreFuncion + "\n";
+    
+        for (int i = 0; i < parametros.length; i++) {
+            resultado += parametros[i] + " = " + argumentos[i] + "\n";
+        }
+    
+        resultado += "Ejecutando el cuerpo de la función: " + cuerpo + "\n";
+    
+        return resultado;
+    }
+    
+}
 
