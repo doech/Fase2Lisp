2c1be9178abc0b39370db91561d86798d90a7fa2
EmilyGG24308 <gon24308@uvg.edu.gt>
2025-03-14 09:55:28 -0600

    Update Expresion.java

--- main/Expresion.java
+++ main/Expresion.java
@@ -1,1 +1,27 @@
+abstract class Expresion {
+    public abstract String evaluar();
+    public abstract boolean verificar();
+}
 
+class ExPredicado extends Expresion {
+    private String operador;
+    private List<Expresion> argumentos = new ArrayList<>();
+
+    public ExPredicado(String operador) {
+        this.operador = operador;
+    }
+
+    public void agregarArgumento(Expresion expr) {
+        argumentos.add(expr);
+    }
+
+    @Override
+    public String evaluar() {
+        return "";
+    }
+
+    @Override
+    public boolean verificar() {
+        return !argumentos.isEmpty();
+    }
+}
