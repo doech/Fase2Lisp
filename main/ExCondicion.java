4ceb8a10ba1ac7ccee917f13d3e17109f33331e7
EmilyGG24308 <gon24308@uvg.edu.gt>
2025-03-16 01:51:59 -0600

    Update ExCondicion.java

--- main/ExCondicion.java
+++ main/ExCondicion.java
@@ -1,1 +1,23 @@
+class ExCondicion implements Expresion {
+    private Expresion condicion;
+    private Expresion siTrue;
+    private Expresion siFalse;
+
+    public ExCondicion(Expresion condicion, Expresion siTrue, Expresion siFalse) {
+        this.condicion = condicion;
+        this.siTrue = siTrue;
+        this.siFalse = siFalse;
+    }
+
+    @Override
+    public String evaluar() {
+        return condicion.evaluar().equals("true") ? siTrue.evaluar() : siFalse.evaluar();
+    }
+
+    @Override
+    public boolean verificar() {
+        return condicion != null && siTrue != null && siFalse != null;
+    }
+}
+
 
