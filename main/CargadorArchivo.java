bb209937564924946602e3a75227f46c4b5a27d7
ale sierra <malesierra15@gmail.com>
2025-03-20 20:50:52 -0600

    ultimos cambios

--- main/CargadorArchivo.java
+++ main/CargadorArchivo.java
@@ -34,120 +34,122 @@ public class CargadorArchivo {
         }
         
         if (!entrada.startsWith("(")) {
-            System.out.println("Error: Expression must start with '('");
+            System.out.println("Error de parentesis falta '('");
             return;
         }
-            StringBuilder expressionBuilder = new StringBuilder();
-            for (int i = 1; i < entrada.length() - 1; i++) {
-                expressionBuilder.append(entrada.charAt(i));
-            }
-            String expression = expressionBuilder.toString().trim();
+
+        if (!entrada.endsWith(")")) {
+            System.out.println("Error de parentesis falta ')");
+            return;
+        }
+
+        String expression = entrada.substring(1, entrada.length() - 1).trim();
             
-            String[] tokens = expression.split("\\s+");
-            if (tokens.length == 0) {
-                System.out.println("Entrada vacía");
-                return;
-            }
+        String[] tokens = expression.split("\\s+");
+        if (tokens.length == 0) {
+            System.out.println("Entrada vacía");
+            return;
+        }
             
-            String operador = tokens[0];
-            switch (operador) {
-                case "setq":
-                    if (tokens.length < 3) {
-                        System.out.println("Error de sintaxis en setq");
-                    } else {
-                        String nombre = tokens[1];
-                        String valor = tokens[2];
-                        ExSet set = new ExSet(nombre, valor, entornoVariables);
-                        String resultadoSetq = set.evaluar();
-                        System.out.println("Variable definida: " + nombre + " = " + resultadoSetq);
-                    }
-                    break;
-                case "cond":
-                    ExCondicion condicion = new ExCondicion(expression, entornoVariables);
-                    String resultadoCondicion = condicion.evaluar();
-                    System.out.println("Resultado: " + resultadoCondicion);
-                    break;
+        String operador = tokens[0];
+        switch (operador) {
+            case "setq":
+                if (tokens.length < 3) {
+                    System.out.println("Error de sintaxis en setq");
+                } else {
+                    String nombre = tokens[1];
+                    String valor = tokens[2];
+                    ExSet set = new ExSet(nombre, valor, entornoVariables);
+                    String resultadoSetq = set.evaluar();
+                    System.out.println("Variable definida: " + nombre + " = " + resultadoSetq);
+                }
+                break;
+            case "cond":
+                ExCondicion condicion = new ExCondicion(expression, entornoVariables);
+                String resultadoCondicion = condicion.evaluar();
+                System.out.println("Resultado: " + resultadoCondicion);
+                break;
 
-                case "<":
-                case ">":
-                case "equal":
-                    if (tokens.length < 3) {
-                        System.out.println("Error de sintaxis en " + operador);
-                    } else {
-                        String valorIzq;
-                        if (entornoVariables.containsKey(tokens[1])) {
-                            valorIzq = entornoVariables.get(tokens[1]);
-                        } else {
-                            valorIzq = tokens[1];
-                        }
-
-                        String valorDer;
-                        if (entornoVariables.containsKey(tokens[2])) {
-                            valorDer = entornoVariables.get(tokens[2]);
-                        } else {
-                            valorDer = tokens[2];
-                        }
-
-                        ExPredicado predicado = new ExPredicado(operador, valorIzq, valorDer, entornoVariables);
-                        String resultadoPredicado = predicado.evaluar();
-                        System.out.println("Resultado: " + resultadoPredicado);
-                    }
-                    break;
-
-                case "quote":
-                    if (tokens.length < 2) {
-                        System.out.println("Error: Sintaxis incorrecta en quote.");
-                        return;
-                    }
-                    String resultadoQuote = expression.substring(6).trim();
-                    System.out.println("Resultado: " + resultadoQuote);
-                    break;
-
-                case "+": 
-                case "-": 
-                case "*": 
-                case "/":
-                    if (tokens.length != 3) {
-                        System.out.println("Error: Operación aritmética inválida");
-                        return;
-                    }
-
-                    String operandoIz;
+            case "<":
+            case ">":
+            case "equal":
+                if (tokens.length < 3) {
+                    System.out.println("Error de sintaxis en " + operador);
+                } else {
+                    String valorIzq;
                     if (entornoVariables.containsKey(tokens[1])) {
-                        operandoIz = entornoVariables.get(tokens[1]);
+                        valorIzq = entornoVariables.get(tokens[1]);
                     } else {
-                        operandoIz = tokens[1];
+                        valorIzq = tokens[1];
                     }
 
-                    String operandoDer;
+                    String valorDer;
                     if (entornoVariables.containsKey(tokens[2])) {
-                        operandoDer = entornoVariables.get(tokens[2]);
+                        valorDer = entornoVariables.get(tokens[2]);
                     } else {
-                        operandoDer = tokens[2];
+                        valorDer = tokens[2];
                     }
 
-                    ExAritmeticas aritmetica = new ExAritmeticas(operador, operandoIz, operandoDer, entornoVariables);
-                    String resultadoAritmetica = aritmetica.evaluar();
-                    System.out.println("Resultado: " + resultadoAritmetica);
-                    break;
+                    ExPredicado predicado = new ExPredicado(operador, valorIzq, valorDer, entornoVariables);
+                    String resultadoPredicado = predicado.evaluar();
+                    System.out.println("Resultado: " + resultadoPredicado);
+                }
+                break;
 
-                case "defun":
-                    if (tokens.length < 4) {
-                        System.out.println("Error de sintaxis en defun");
-                    } else {
-                        String nombreFuncion = tokens[1];
-                        String parametros = tokens[2]; 
-                        String cuerpo = tokens[3];     
+            case "quote":
+                if (tokens.length < 2) {
+                    System.out.println("Error: Sintaxis incorrecta en quote.");
+                    return;
+                }
+                String resultadoQuote = expression.substring(6).trim();
+                System.out.println("Resultado: " + resultadoQuote);
+                break;
 
-                        Funciones nuevaFuncion = ExFunciones.definirFuncion(nombreFuncion, parametros, cuerpo);
-                        funciones.put(nombreFuncion, nuevaFuncion);
+            case "+": 
+            case "-": 
+            case "*": 
+            case "/":
+                if (tokens.length != 3) {
+                    System.out.println("Error: Operación aritmética inválida");
+                    return;
+                }
 
-                        System.out.println("Función " + nombreFuncion + " definida correctamente");
-                    }
-                    break;
+                String operandoIz;
+                if (entornoVariables.containsKey(tokens[1])) {
+                    operandoIz = entornoVariables.get(tokens[1]);
+                } else {
+                    operandoIz = tokens[1];
+                }
 
-                default:
-                 System.out.println("Error: Operador no reconocido - " + operador);
-            }
+                String operandoDer;
+                if (entornoVariables.containsKey(tokens[2])) {
+                    operandoDer = entornoVariables.get(tokens[2]);
+                } else {
+                    operandoDer = tokens[2];
+                }
+
+                ExAritmeticas aritmetica = new ExAritmeticas(operador, operandoIz, operandoDer, entornoVariables);
+                String resultadoAritmetica = aritmetica.evaluar();
+                System.out.println("Resultado: " + resultadoAritmetica);
+                break;
+
+            case "defun":
+                if (tokens.length < 4) {
+                    System.out.println("Error de sintaxis en defun");
+                } else {
+                    String nombreFuncion = tokens[1];
+                    String parametros = tokens[2]; 
+                    String cuerpo = tokens[3];     
+
+                    Funciones nuevaFuncion = ExFunciones.definirFuncion(nombreFuncion, parametros, cuerpo);
+                    funciones.put(nombreFuncion, nuevaFuncion);
+
+                    System.out.println("Función " + nombreFuncion + " definida correctamente");
+                }
+                break;
+
+            default:
+                System.out.println("Error: Operador no reconocido - " + operador);
+        }
     }
 }
